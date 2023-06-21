687c650f9f10f624fc3f23ebf417cd72f78bf1c4
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-06-21 19:33:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
-const line = document.getElemetById('line');
+const line = document.getElementById('line');
 let rotation = 0;
 
 setInterval( () => {
 	rotation += 5;
-	line.style.transform = `translate(-50%, 50%) rotate(${rotation}deg)`;
+	line.style.transform = `translate(-50%, -50%) rotate(${rotation}deg)`;
 }, 50);
